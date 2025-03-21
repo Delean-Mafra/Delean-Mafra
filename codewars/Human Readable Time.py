5183492f40b8aac72b329be44c4a0c0f95bf1875
Delean Mafra <170483770+Delean-Mafra@users.noreply.github.com>
2025-03-20 22:39:02 -0300

    Update Human Readable Time.py
    Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
    
    HH = hours, padded to 2 digits, range: 00 - 99
    MM = minutes, padded to 2 digits, range: 00 - 59
    SS = seconds, padded to 2 digits, range: 00 - 59
    The maximum time never exceeds 359999 (99:59:59)
    
    You can find some examples in the test fixtures.
    
    #Sample Tests
    import codewars_test as test
    from solution import make_readable
    
    @test.describe("Fixed Tests")
    def fixed_tests():
        @test.it('Basic Test Cases')
        def basic_test_cases():
            test.assert_equals(make_readable(0), "00:00:00")
            test.assert_equals(make_readable(59), "00:00:59")
            test.assert_equals(make_readable(60), "00:01:00")
            test.assert_equals(make_readable(3599), "00:59:59")
            test.assert_equals(make_readable(3600), "01:00:00")
            test.assert_equals(make_readable(86399), "23:59:59")
            test.assert_equals(make_readable(86400), "24:00:00")
            test.assert_equals(make_readable(359999), "99:59:59")

--- codewars/Human Readable Time.py
+++ codewars/Human Readable Time.py
@@ -1,31 +1,3 @@
-"""
-Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
-
-HH = hours, padded to 2 digits, range: 00 - 99
-MM = minutes, padded to 2 digits, range: 00 - 59
-SS = seconds, padded to 2 digits, range: 00 - 59
-The maximum time never exceeds 359999 (99:59:59)
-
-You can find some examples in the test fixtures.
-
-#Sample Tests
-import codewars_test as test
-from solution import make_readable
-
-@test.describe("Fixed Tests")
-def fixed_tests():
-    @test.it('Basic Test Cases')
-    def basic_test_cases():
-        test.assert_equals(make_readable(0), "00:00:00")
-        test.assert_equals(make_readable(59), "00:00:59")
-        test.assert_equals(make_readable(60), "00:01:00")
-        test.assert_equals(make_readable(3599), "00:59:59")
-        test.assert_equals(make_readable(3600), "01:00:00")
-        test.assert_equals(make_readable(86399), "23:59:59")
-        test.assert_equals(make_readable(86400), "24:00:00")
-        test.assert_equals(make_readable(359999), "99:59:59")
-"""
-
 def make_readable(seconds):
     print('Copyright © Delean Mafra, todos os direitos reservados | All rights reserved.')
     """
