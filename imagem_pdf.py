77b1d5b9bda05d3c7444e51a4e46724174b081d0
Delean Mafra <170483770+Delean-Mafra@users.noreply.github.com>
2025-04-21 21:51:10 -0300

    Add files via upload

--- imagem_pdf.py
+++ imagem_pdf.py
@@ -1,14 +1,12 @@
 from PIL import Image
 
-print("Copyright ©2025 | Delean Mafra, todos os direitos reservados.")
-
 # Abre o arquivo de imagem
-img = Image.open(r'\Sem título.png')
+img = Image.open(r'D:\Python\Python_projcts\templates\Sem título.png')
 
 # Converte a imagem para o modo RGB
 img = img.convert('RGB')
 
 # Salva a imagem como PDF
-img.save(r'arquivo.pdf')
+img.save(r'C:\Users\Acer\Desktop\certificado.pdf')
 
 print("A imagem foi convertida com sucesso para output.pdf")
