f045f84cf1f2d2218719c6ae9e689f206c5296f2
Delean Mafra <170483770+Delean-Mafra@users.noreply.github.com>
2026-09-01 22:35:24 -0300

    Delete codewars directory

--- codewars/Filtering even numbers (Bug Fixes).py
+++
@@ -1,3 +0,0 @@
-def kata_13_december(lst):
-    #Copyright © Delean Mafra, todos os direitos reservados
-    return list(filter(lambda x: x % 2 != 0, lst))
--- codewars/Pensando e testando Quem é você.py
+++
@@ -1,5 +0,0 @@
-print("Copyright ©2025 | Delean Mafra, todos os direitos reservados.")
-
-def testit(a, b):
-    # My name is Delean
-    return a+b
